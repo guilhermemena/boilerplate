d6ede9c94c6e8b0a28265a4fdd0b1e8be9f4f1fb
Guilherme Mena <guilherme.atu@gmail.com>
2021-04-01 19:39:02 -0300

    chore: add eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -50,7 +51,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={styles.footer  }>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
