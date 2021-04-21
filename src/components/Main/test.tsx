27e1acaa103c9b5942dec38e57a7cd1169c8b433
Guilherme Mena <guilherme.atu@gmail.com>
2021-04-20 22:53:56 -0300

    test: ensure to be able to render the correctly background color

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from '@testing-library/react';
 
-import Main from 'src/components/Main/';
+import Main from './index';
 
 describe('<Main />', () => {
   it('should render the heading', () => {
     render(<Main />);
 
     expect(screen.getByRole('heading', { name: /next js/i })).toBeInTheDocument();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': 'hsl(223, 63%, 36%)' });
   });
 });
